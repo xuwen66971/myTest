7364f2e942ef4d79491e8e938c516a76eb4cdc3d
“xuwen <646385329@qq.com>
2018-05-23 16:41:00 +0800

    测试

---
+++ springbootdemo/src/main/java/Author.java
@@ -0,0 +1,18 @@
+/****
+ * @author: xuwen
+ * @date: 2018/5/22 20:16
+ * @descripte:
+ **/
+public class Author {
+
+    private Integer money;
+
+    @Override
+    public String toString() {
+
+        return "Author{" +
+                "money=" + money +
+                '}';
+    }
+
+}
---
+++ springbootdemo/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+hello word! <br>
+baby
+</body>
+</html>
